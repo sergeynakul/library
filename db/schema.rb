a02dd738444d6c3882bd39a8528cb3aef4742a5f
Sergio <nakul.sv@gmail.com>
2019-10-15 20:30:42 +0300

    assignment of roles for admins

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_191_014_133_935) do
+ActiveRecord::Schema.define(version: 20_191_015_162_240) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20_191_014_133_935) do
     t.datetime 'remember_created_at'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.string 'role', null: false
     t.index ['email'], name: 'index_admin_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_admin_users_on_reset_password_token', unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 if Rails.env.development?
-  AdminUser.create!(email: 'admin@example.com', password: '123456789', password_confirmation: '123456789')
-  first_group = Group.create!(name: 'Comedy')
-  second_group = Group.create!(name: 'Fantasy')
-  first_book = Book.create!(title: 'First comedy book', group_id: first_group.id)
-  second_book = Book.create!(title: 'Second comedy book', group_id: first_group.id)
-  third_book = Book.create!(title: 'First fantasy book', group_id: second_group.id)
-  first_author = Author.create!(name: 'First author')
-  second_author = Author.create!(name: 'Second author')
-  first_author.books << first_book
-  first_author.books << second_book
-  second_author.books << third_book
+  AdminUser.create!(email: 'admin@example.com', password: '123456789', password_confirmation: '123456789', role: 'superadmin')
 end
